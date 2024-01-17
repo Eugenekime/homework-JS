cbad3cc40406f65e4d6d2d7d437652a9dbc2c02b
Kim Eugene <kimeuegene0824@gmail.com>
2024-01-17 11:43:29 +0900

    asd

--- 4-navigator/app.js
+++
@@ -1,8 +0,0 @@
-const addressLat = 67;
-const addressLong = 23;
-const positionLat = 51;
-const positionLong = 96;
-
-const distance = Math.sqrt((addressLat - positionLat) ** 2 + (positionLong - addressLong) ** 2);
-
-console.log(distance);
--- 6-driver/app.js
+++
@@ -1,9 +0,0 @@
-let hasLicense = true;
-let age = prompt('Enter your age: ');
-let isDrink = false;
-
-if (hasLicense === true && age >= 18 && isDrink === false) {
-  console.log('You can drive a car');
-} else {
-  console.log('Sorry, you can not drive a car');
-}
